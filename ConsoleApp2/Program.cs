356cbead5c89a4de7a27bd1eae2f74ee1f3e926e
timboehlen <tim.boehlen@nexplore.ch>
2022-12-06 14:50:58 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Aufgabe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Data.input;
+
+            for (int i = 0; i <= input.Length; i++)
+            {
+                string marker = input.Substring(i, 14);
+                int uniqueLetters = marker.Distinct().Count();
+
+                if (uniqueLetters == 14)
+                {
+                    Console.WriteLine(i + 14);
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
